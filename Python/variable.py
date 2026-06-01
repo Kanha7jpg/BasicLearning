c425a67107ae7e8f1e345b8b95a734f8f66f96b7
kando7 <167471914+Kanha7jpg@users.noreply.github.com>
2026-06-01 14:10:32 +0530

    var

---
+++ Python/variable.py
@@ -0,0 +1,11 @@
+firstmame = "sam"
+lastname = "smith"
+fullname = firstmame + " " + lastname
+print(fullname)
+
+country = "finland"
+city = "helsinki"
+location = country + " " + city
+print(location)
+
+age = 22
